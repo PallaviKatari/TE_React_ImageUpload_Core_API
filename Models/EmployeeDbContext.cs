1f155f3d10357651c1fb799a8893ae6839d1f3d5
PallaviKatari <pallavikatari@gmail.com>
2023-09-30 16:05:53 +0530

    Add project files.

---
+++ Models/EmployeeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace React_ImageUpload_Core_API.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EmployeeModel> Employees { get; set; }
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,31 @@
+﻿#nullable disable
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace React_ImageUpload_Core_API.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string EmployeeName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Occupation { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string ImageName { get; set; }
+
+        [NotMapped]
+        public IFormFile ImageFile { get; set; }
+
+        [NotMapped]
+        public string ImageSrc { get; set; }
+
+        //Pascal(EmployeeName) -> Camel EmployeeID ->employeeID
+        //Camel(employeeName) -> Pascal
+    }
+}
